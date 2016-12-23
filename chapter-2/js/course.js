f32bac8dc4215189c02abba17e6b62256c2e4fd4
Chima <nmerenuchima@hotmail.com>
2016-12-23 20:52:50 +0100

    Moving to chapter 3

--- chapter-2/js/course.js
+++ chapter-2/js/course.js
@@ -1,2 +1,4 @@
-var g = "five" * 2;
-console.log(g);
+var h = "5";
+console.log(h + 1); //Concatentation: shows the string "51"
+h = Number ("5");
+console.log(h + 1); //Addition: show the number 6 
