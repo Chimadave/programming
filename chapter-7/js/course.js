1a5177e59eaad03b592f3929f8bbe4514d6622c1
Chima <nmerenuchima@hotmail.com>
2016-12-24 02:13:37 +0100

    added more pastery

--- chapter-7/js/course.js
+++ chapter-7/js/course.js
@@ -1,22 +1,35 @@
-var cake = {
+/*var cake = {
 	flavor: "vanilla",
 	price: "$10", 
 	layers: 4,
 	occasion:"birthday",
-
+// Describing the cake here 
 	describe: function () {
 	var description ="The " + this.occasion + " cake has a " + this.flavor + " flavor, " + this.layers +
 	" layers, and costs " + this.price + ".";
 	return description;
-	}
+	} // this description method is inside the cake property and hence the use of 'this'
 };
 
+console.log(cake.describe()); // using the method as cake property here 
 
-console.log(cake.describe());
-
-	cake.occasion = "wedding";
+	cake.occasion = "wedding"; // this cake is for wedding 
 	cake.flavor = "strawberry";
-	cake.layers = cake.layers + 8;
+	cake.layers = cake.layers + 8; // number of layers and price go up! 
 	cake.price = "$50";
 
 	console.log(cake.describe());
+	*/
+	var muffin = {
+	type: "muffin"
+	flavor: "blueberry",
+	price: "$10", 
+	layers: 1,
+	occasion:"breakfast",
+// Describing the pastry here 
+	describe: function () {
+	var description ="The " + this.type + " is a " + this.occasion + " pastry, has a " + this.flavor + " flavor, " + this.layers +
+	" layers, and costs " + this.price + ".";
+	return description;
+	} // this description method is inside the cake property and hence the use of 'this'
+};
