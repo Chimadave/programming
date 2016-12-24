a5255837ab5e1c1aba2e90eeb21de619b7ace704
Chima <nmerenuchima@hotmail.com>
2016-12-24 10:26:50 +0100

    using methods in object property and then protyping too

--- chapter-8/js/course.js
+++ chapter-8/js/course.js
@@ -8,6 +8,7 @@ var anotherObject = Object.create(anObject);
 console.log(anotherObject.a);  */
 
 
+/*
 var Pastry = {
 	type: "",
 	flavor: "",
@@ -25,7 +26,7 @@ var Pastry = {
 
 // now cake and muffins will leverage on the prototype of pastry.
 
-var muffin = Object.create(Pastry);
+ var muffin = Object.create(Pastry);
 muffin.type = "muffin";
 muffin.flavor = "blueberry";
 muffin.layers = 1;
@@ -47,9 +48,57 @@ console.log(cake.describe());
 cake.occasion = "wedding"; 
 cake.layers = 12;
 cake.price = "$50";
-cake.flavor = "red-velvet";
+cake.flavor = "red-velvet"; 
+
 cake.type = "cake"
 
 console.log (cake.describe());
 
+ */
+// the object pastry has 2 properties which are methods
+var Pastry = {
+	// initialize the pastry 
+	init: function(type, flavor, layers, price, occasion){ // a method giving the pastry properties
+		this.type = type;
+		this.flavor = flavor;
+		this.layers = layers;
+		this.price = price;
+		this.occasion = occasion;
+	},
 
+	// describe the pastry
+	describe: function(){ // a method describing the pastry
+		var description = "The " + this.type + " is a " + this.occasion + " pastry, has a " + this.flavor + " flavor,  "
+		+ this.layers + " layer(s) and costs " + this.price + "."
+		return description;
+	},
+
+	bake: function () {
+		var baked = "The " + this.flavor + " " + this.type + " was placed in the oven. It's done!"
+	return baked;
+	}, 
+
+	eat: function(){
+		var eaten = "The " + this.flavor + " " + this.type + " has been eaten!!"
+		return eaten; 
+	}
+}; 
+
+var muffin = Object.create(Pastry);
+muffin.init ("muffin", "blueberry", 1, "$2", "breakfast");
+
+var cake = Object.create(Pastry);
+cake.init("cake", "vanilla", 3, "$10", "birthday");
+
+console.log(muffin.bake()); 
+console.log(muffin.describe());
+console.log(muffin.eat());
+
+console.log(cake.bake());
+console.log(cake.describe());
+console.log(cake.eat()); 
+
+cake.init("cake", "red-velvet", 12, "$50", "wedding"); 
+console.log(cake.bake());
+console.log(cake.describe());
+console.log(cake.eat());
